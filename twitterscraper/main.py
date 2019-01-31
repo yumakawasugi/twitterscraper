523da2967873674788d636368054d85db4bba6f0
yumakawasugi <kawasugi-96@outlook.jp>
2019-02-01 00:51:02 +0900

    change

--- twitterscraper/main.py
+++ twitterscraper/main.py
@@ -31,7 +31,7 @@ class JSONEncoder(json.JSONEncoder):
 
 def valid_date(s):
     try:
-        return dt.datetime.strptime(s, "%Y-%m-%dT%H").datetime()
+        return dt.strptime(s, "%Y-%m-%dT%H")
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
